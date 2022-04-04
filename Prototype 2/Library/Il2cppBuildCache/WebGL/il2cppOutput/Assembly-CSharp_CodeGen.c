83b8b2b43328b541ac6489793c8e53386cc9be9e
asdian99 <79957342+asdian99@users.noreply.github.com>
2022-04-04 23:31:01 +0700

    Tugas 2

---
+++ Prototype 2/Library/Il2cppBuildCache/WebGL/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,111 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void DestroyOutOfBounds::Start()
+extern void DestroyOutOfBounds_Start_mF1982FE2F4AF5912172A4359E1C65ED580ABD67C (void);
+// 0x00000002 System.Void DestroyOutOfBounds::Update()
+extern void DestroyOutOfBounds_Update_m2337730029C731551291E932F658C0E098AAFE59 (void);
+// 0x00000003 System.Void DestroyOutOfBounds::.ctor()
+extern void DestroyOutOfBounds__ctor_mB18D293D861B36C889E2E31B07673E10C0CDD5DC (void);
+// 0x00000004 System.Void DetectCollisions::Start()
+extern void DetectCollisions_Start_mAE7A4B06ADF423C95E1DA751AD71E0D047552B4F (void);
+// 0x00000005 System.Void DetectCollisions::Update()
+extern void DetectCollisions_Update_mCCAE269E4C3B9BD943E227B49AED8F542F3A7B5C (void);
+// 0x00000006 System.Void DetectCollisions::OnTriggerEnter(UnityEngine.Collider)
+extern void DetectCollisions_OnTriggerEnter_m4EEFDB371DEBDBF5A256720A3B8F1F4B35112704 (void);
+// 0x00000007 System.Void DetectCollisions::.ctor()
+extern void DetectCollisions__ctor_mE4563DA16E143370A82855DDDF2C4C84B265B473 (void);
+// 0x00000008 System.Void MoveForward::Start()
+extern void MoveForward_Start_m01A14393269317C9EC0DE9C8B34C219475B5B9D0 (void);
+// 0x00000009 System.Void MoveForward::Update()
+extern void MoveForward_Update_m1D1EA1D175B6E5FED262E5D66BD0F05A5A6C1B1E (void);
+// 0x0000000A System.Void MoveForward::.ctor()
+extern void MoveForward__ctor_m79E20E057C5117B7FC19B2CB3146FCAF61ABE123 (void);
+// 0x0000000B System.Void PlayerController::Start()
+extern void PlayerController_Start_m9531F30EC892BDD1758A2EEC724E86EFBDA150A3 (void);
+// 0x0000000C System.Void PlayerController::Update()
+extern void PlayerController_Update_mB31159CAD7DD2329859472554BC9154A83D8E794 (void);
+// 0x0000000D System.Void PlayerController::.ctor()
+extern void PlayerController__ctor_mF30385729DAFDFCB895C4939F6051DCE6C0327FB (void);
+// 0x0000000E System.Void SpawnManager::Start()
+extern void SpawnManager_Start_mFA050B5C59C7CC47F4430CA0F5A646051D5442CB (void);
+// 0x0000000F System.Void SpawnManager::Update()
+extern void SpawnManager_Update_m68C1739F474F0D29A0BF533BD44BA82786A3C96B (void);
+// 0x00000010 System.Void SpawnManager::SpawnRandomAnimal()
+extern void SpawnManager_SpawnRandomAnimal_m6FEC50772F07738073875335C14F1E6BD2414FD0 (void);
+// 0x00000011 System.Void SpawnManager::.ctor()
+extern void SpawnManager__ctor_mBCD48EEAB1EB733A88D47C85ACC373C796F20E59 (void);
+static Il2CppMethodPointer s_methodPointers[17] = 
+{
+	DestroyOutOfBounds_Start_mF1982FE2F4AF5912172A4359E1C65ED580ABD67C,
+	DestroyOutOfBounds_Update_m2337730029C731551291E932F658C0E098AAFE59,
+	DestroyOutOfBounds__ctor_mB18D293D861B36C889E2E31B07673E10C0CDD5DC,
+	DetectCollisions_Start_mAE7A4B06ADF423C95E1DA751AD71E0D047552B4F,
+	DetectCollisions_Update_mCCAE269E4C3B9BD943E227B49AED8F542F3A7B5C,
+	DetectCollisions_OnTriggerEnter_m4EEFDB371DEBDBF5A256720A3B8F1F4B35112704,
+	DetectCollisions__ctor_mE4563DA16E143370A82855DDDF2C4C84B265B473,
+	MoveForward_Start_m01A14393269317C9EC0DE9C8B34C219475B5B9D0,
+	MoveForward_Update_m1D1EA1D175B6E5FED262E5D66BD0F05A5A6C1B1E,
+	MoveForward__ctor_m79E20E057C5117B7FC19B2CB3146FCAF61ABE123,
+	PlayerController_Start_m9531F30EC892BDD1758A2EEC724E86EFBDA150A3,
+	PlayerController_Update_mB31159CAD7DD2329859472554BC9154A83D8E794,
+	PlayerController__ctor_mF30385729DAFDFCB895C4939F6051DCE6C0327FB,
+	SpawnManager_Start_mFA050B5C59C7CC47F4430CA0F5A646051D5442CB,
+	SpawnManager_Update_m68C1739F474F0D29A0BF533BD44BA82786A3C96B,
+	SpawnManager_SpawnRandomAnimal_m6FEC50772F07738073875335C14F1E6BD2414FD0,
+	SpawnManager__ctor_mBCD48EEAB1EB733A88D47C85ACC373C796F20E59,
+};
+static const int32_t s_InvokerIndices[17] = 
+{
+	897,
+	897,
+	897,
+	897,
+	897,
+	776,
+	897,
+	897,
+	897,
+	897,
+	897,
+	897,
+	897,
+	897,
+	897,
+	897,
+	897,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	17,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
